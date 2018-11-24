17d07f580b825726859d3551d18870a72889179f
meepoljd <jiandong.liu93@gmail.com>
2018-11-24 12:34:00 +0000

    use tsinghua mirrors

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -10,19 +10,21 @@
 
 
 ;;; Standard package repositories
-
-(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
-       (proto (if no-ssl "http" "https")))
-  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  ;; Official MELPA Mirror, in case necessary.
-  ;;(add-to-list 'package-archives (cons "melpa-mirror" (concat proto "://www.mirrorservice.org/sites/melpa.org/packages/")) t)
-  (if (< emacs-major-version 24)
-      ;; For important compatibility libraries like cl-lib
-      (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))
-    (unless no-ssl
-      ;; Force SSL for GNU ELPA
-      (setcdr (assoc "gnu" package-archives) "https://elpa.gnu.org/packages/"))))
+(setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+			 ("org-cn"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/")
+                         ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+;; (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+;;                     (not (gnutls-available-p))))
+;;        (proto (if no-ssl "http" "https")))
+;;   (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+;;   ;; Official MELPA Mirror, in case necessary.
+;;   ;;(add-to-list 'package-archives (cons "melpa-mirror" (concat proto "://www.mirrorservice.org/sites/melpa.org/packages/")) t)
+;;   (if (< emacs-major-version 24)
+;;       ;; For important compatibility libraries like cl-lib
+;;       (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))
+;;     (unless no-ssl
+;;       ;; Force SSL for GNU ELPA
+;;       (setcdr (assoc "gnu" package-archives) "https://elpa.gnu.org/packages/"))))
 
 
 ;;; On-demand installation of packages
